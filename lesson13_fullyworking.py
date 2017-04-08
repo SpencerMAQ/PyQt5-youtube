da273fd41542be35a2f5b90bcbc661b88d9d25d8
Spencer <spencer.michael.q@gmail.com>
2017-04-08 15:26:18 +0800

    ..

--- lesson13_fullyworking.py
+++ lesson13_fullyworking.py
@@ -1,10 +1,11 @@
-from PyQt5.QtWidgets import (QApplication, QTextEdit, QWidget, QPushButton, QVBoxLayout)          # QtGui for images
+from PyQt5.QtWidgets import (QApplication, QTextEdit, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QFileDialog)          # QtGui for images
 import sys
+import os
 
 
 '''
-Lesson 12: (1/5) TextEdit
-(2/5) Save Files
+Lesson 13: (1/5) Simple Text Editor with QFIleDialog
+(3/5) Save Files
 File Dialog, SOrted Dialog
 Menu Bar
 Put all together for Notepad program
@@ -18,28 +19,50 @@ class Notepad(QWidget):
 
         super(Notepad, self).__init__()                  # this is essential the same as w = QTWidgets.QWidget() where init happens automatically
         self.text = QTextEdit(self)
-        self.clr_btn = QPushButton('Save')
+        self.clr_btn = QPushButton('Clear')
+        self.sav_btn = QPushButton('Save')      # added 2 more buttons from prev
+        self.opn_btn = QPushButton('Open')
 
         self.init_ui()
         # return self, text, clr_btn
 
     def init_ui(self):
-        layout = QVBoxLayout()
-        layout.addWidget(self.text)
-        layout.addWidget(self.clr_btn)
+        v_layout = QVBoxLayout()
+        h_layout = QHBoxLayout()
 
-        self.clr_btn.clicked.connect(self.save_text)
+        h_layout.addWidget(self.clr_btn)
+        h_layout.addWidget(self.sav_btn)
+        h_layout.addWidget(self.opn_btn)
 
-        self.setLayout(layout)
+
+        v_layout.addWidget(self.text)
+       # v_layout.addWidget(self.clr_btn)  # previous vid
+        v_layout.addLayout(h_layout)
+
+        self.sav_btn.clicked.connect(self.save_text)
+        self.clr_btn.clicked.connect(self.clear_text)   # new
+        self.opn_btn.clicked.connect(self.open_text)       #new
+
+        self.setLayout(v_layout)
         self.setWindowTitle('Lesson11: QTextEdit')
 
         self.show()
 
     def save_text(self):
-        with open('test.txt', 'w') as fh:
+        # args = (self, __title of Dialog Box__, )
+        filename = QFileDialog.getSaveFileName(self, 'Save File', os.getenv('HOME'))
+        with open(filename[0], 'w') as fh:      # QFileDialog.get.... returns a tuple, you only want the first
             my_text = self.text.toPlainText()
             fh.write(my_text)
 
+    def clear_text(self):   # new from prev vid
+        self.text.clear()
+
+    def open_text(self):
+        filename = QFileDialog.getOpenFileName(self, 'Open File', os.getenv('HOME'))
+        with open(filename[0], 'r') as fh:      # QFileDialog.get.... returns a tuple, you only want the first
+            file_text = fh.read()
+            self.text.setText(file_text)
 
 app = QApplication([])
 a_window = Notepad()
